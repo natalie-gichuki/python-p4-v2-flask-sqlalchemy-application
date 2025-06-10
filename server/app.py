c5932d167c0bf63911d5cca038f8ec3df00f6aa4
NatalieGichuki <nataliegichuki@gmail.com>
2025-06-10 20:46:58 +0300

    Complete codealong

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Pet
@@ -15,6 +15,52 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 # add views here 
+@app.route('/')
+def index():
+    response = make_response(
+        '<h1>Welcome to the pet directory!</h1>',
+        200
+    )
+    return response
+
+# ✅ Route to list all pets
+@app.route('/pets')
+def all_pets():
+    pets = Pet.query.all()
+    pet_list = [ {'id': pet.id, 'name': pet.name, 'species': pet.species} for pet in pets ]
+    return jsonify(pet_list), 200
+
+
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    if pet:
+        response_body = f'<p>{pet.name} {pet.species}</p>'
+        response_status = 200
+    else:
+        response_body = f'<p>Pet {id} not found</p>'
+        response_status = 404
+
+    response = make_response(response_body, response_status)
+    return response
+
+@app.route('/species')
+def all_species():
+    species = db.session.query(Pet.species).distinct().all()
+    species_list = [s[0] for s in species]
+    return jsonify(species_list), 200
+
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets = Pet.query.filter_by(species=species).all()
+
+    size = len(pets)  # all() returns a list so we can get length
+    response_body = f'<h2>There are {size} {species}s</h2>'
+    for pet in pets:
+        response_body += f'<p>{pet.name}</p>'
+    response = make_response(response_body, 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
